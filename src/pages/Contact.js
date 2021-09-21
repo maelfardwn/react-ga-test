4ad65527d6db73e143f2b6c2c59c873e6f411786
maelfardwn <maelfa011@gmail.com>
2021-09-21 15:35:28 +0700

    re add packag3

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,10 +8,26 @@ class Contact extends Component {
   render(){
   return(
     <div>
-      
-        <ReactTitle title="Page 2"/>
-          <div className="content"> Some Content </div>
-        
+        <Helmet>
+            
+          <title>Contact Title</title>
+          <meta name="title" content="Contact Title"/>
+          <meta name="description" content="This is descripttion of contact page"/>
+          {/* 
+          <!-- Open Graph / Facebook --> */}
+          <meta property="og:type" content="website"/>
+          <meta property="og:url" content="https://assesmenttestridwan.herokuapp.com/contact"/>
+          <meta property="og:title" content="Contact Title OG"/>
+          <meta property="og:description" content="This is descripttion of contact page OG"/>
+          <meta property="og:image" content="https://assesmenttestridwan.herokuapp.com/assets/contact.png"/>
+          {/* 
+          <!-- Twitter --> */}
+          <meta property="twitter:card" content="summary_large_image"/>
+          <meta property="twitter:url" content="https://assesmenttestridwan.herokuapp.com/contact"/>
+          <meta property="twitter:title" content="Contact Title Twitter"/>
+          <meta property="twitter:description" content="This is descripttion of contact page Twitter"/>
+          <meta property="twitter:image" content="https://assesmenttestridwan.herokuapp.com/assets/contact.png"/>
+        </Helmet>
         <h1>this is the Contact Page</h1>
     </div>
   )
