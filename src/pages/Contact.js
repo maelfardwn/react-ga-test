e963e1e76696a2b689a56582099e0cf07263a4c5
maelfardwn <maelfa011@gmail.com>
2021-09-21 09:11:35 +0700

    sec commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import MetaDecorator from '../components/MetaDecorator'
+import contact from '../assets/contact.png'
 
 export default function Contact(){
   return(
     <div>
+    <MetaDecorator description="This is Ridwan test Contact opengraph" title="opengraph Test Contact" imageUrl={contact} imageAlt="contact with contact image"/>
       <h1>this is the Contact Page</h1>
     </div>
   )
