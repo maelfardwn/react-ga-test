f909a30bb9a21211cfc51df9a083b73f66e18322
maelfardwn <maelfa011@gmail.com>
2021-09-21 13:01:04 +0700

    re add packageee

--- src/index.js
+++ src/index.js
@@ -2,20 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import {hydrate, render} from 'react-dom'
 import reportWebVitals from './reportWebVitals';
-import Routes from './routes';
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes />
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+import Home from './pages/Home';
+const APP = (
+  <BrowserRouter>
+  <Switch>
+      <Route path='/about' component = { About } />
+      <Route path='/projects' component = { Projects } />
+      <Route path='/contact' component = { Contact }/>
+      <Route exact path='/' component={Home} />
+    </Switch>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
+const rootElemet = document.getElementById("root");
+if(rootElemet.hasChildNodes()){
+hydrate(APP, rootElemet)
+}else{
+  render(APP, rootElemet)
+}
