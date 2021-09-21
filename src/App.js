17f84fc32ea8ced9777490b8a05cba7756b0cdda
maelfardwn <maelfa011@gmail.com>
2021-09-21 11:55:58 +0700

    third com

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import ReactGa from 'react-ga'
 import { useEffect } from 'react';
 import {Helmet} from 'react-helmet';
 
+import london from './assets/london.jpg'
+import { Link } from 'react-router-dom';
 function App() {
 
   useEffect(()=>{
@@ -15,9 +17,33 @@ function App() {
 
   return (
     <div className="App">
+    <nav>
+      <Link to="/contact"/>
+      <Link to="/about"/>
+      <Link to="/project"/>
 
-    <Navbar />
-   <Routes />
+    </nav>
+   <Helmet>
+
+    <meta name="description" content="Description this "/>
+    
+    <meta property="og:title" content='opengraph test homepage'/>
+    <meta property="og:description" content='This is homepage using opengraph for testing'/>
+    <meta property="og:description" content="Description this"/>
+    <meta property="og:image" content='https://assesmenttestridwan.herokuapp.com/assets/london.jpg'/>
+    <meta property="og:url" content='https://assesmenttestridwan.herokuapp.com/'/>
+    
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:image:alt" content='London Pict With Twitter image alt'/> 
+    <meta property="twitter:description" content="Description this"/>
+    </Helmet>
+    <header className="App-header">
+      <div style={{width:'400px',height:'200px'}}>
+      <img width='200px' height='100px' src={london}/>
+       <h2>Hello</h2>
+       <p>This is home page . and you will see more information from another page</p>
+      </div>
+      </header>
       
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom'
-
+import { BrowserRouter ,Switch, Route} from 'react-router-dom'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+import { hydrate, render } from "react-dom";
+import App from './App';
+ 
+const APP = (
+  <BrowserRouter>
+  <Switch>
+    <Route exact path='/' component={App} />
+    <Route exact path='/about' component = { About } />
+    <Route exact path='/project' component = { Projects } />
+    <Route exact path='/contact' component = { Contact }/>
+  </Switch>
+  </BrowserRouter>
+
+)
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(APP, rootElement);
+} else {
+  render(APP, rootElement);
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
